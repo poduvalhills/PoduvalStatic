30270047eaa6072eba3c77352b10770de7bd99c4
PriyadharshanRavi <mailmefriends95@gmail.com>
2019-07-23 12:49:41 +0530

    Email added to contact

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,6 +1,6 @@
 import React from 'react';
 //import './contact.css';
-import { MdPhoneInTalk } from "react-icons/md";
+import { MdPhoneInTalk, MdEmail } from "react-icons/md";
 import Jump from 'react-reveal/Jump';
 
 class Contact extends React.Component{
@@ -13,7 +13,8 @@ class Contact extends React.Component{
                         <div className="col-md-12 text-center">
                             <h5>Contact</h5><br/>
                             <h6>Poduval - <MdPhoneInTalk /> 9884478888 </h6> 
-                            <h6>Suresh Chandra Menon - <MdPhoneInTalk /> 9884126646 </h6><br/>   
+                            <h6>Suresh Chandra Menon - <MdPhoneInTalk /> 9884126646 </h6>
+                            <h6>E-Mail - <a href="mailto:poduvalhills@gmail.com" style={{color: 'black'}}><MdEmail /></a> poduvalhills@gmail.com </h6><br/>   
                             <small><center> &copy; 2019 </center></small>
                         </div>
                     </div>
