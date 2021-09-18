211dd97eedb968a68d6c244d63d5105167c19e23
poduvalhills <56753005+poduvalhills@users.noreply.github.com>
2021-09-18 12:11:00 +0530

    Update Videogallery.js
    video path update

--- src/components/video/Videogallery.js
+++ src/components/video/Videogallery.js
@@ -8,27 +8,24 @@ class Videogallery extends React.Component{
                 <div className="row" id="videos">
                     <Fade left>
                         <div className="col-md-4 text-center">
-                            <video width="400" controls>
-                              <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4" type="video/mp4" />
-                              <!--<source src={require('../media/checkdam.mp4')} type="video/mp4" />-->
+                            <video width="400" controls>                              
+                              <source src={require('../media/checkdam.mp4')} type="video/mp4" />
                               Your browser does not support HTML5 video.
                             </video>
                         </div>
                     </Fade>
                     <Fade left>
                         <div className="col-md-4 text-center">
-                            <video width="400" controls>
-                              <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4" type="video/mp4" />
-                              <!--<source src={require('../media/hills.mp4')} type="video/mp4" />-->
+                            <video width="400" controls>                              
+                              <source src={require('../media/hills.mp4')} type="video/mp4" />
                               Your browser does not support HTML5 video.
                             </video>
                         </div>
                     </Fade>
                     <Fade left>
                         <div className="col-md-4 text-center">
-                            <video width="400" controls>
-                              <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4" type="video/mp4" />
-                              <!--<source src={require('../media/views.mp4')} type="video/mp4" />-->
+                            <video width="400" controls>                              
+                              <source src={require('../media/views.mp4')} type="video/mp4" />
                               Your browser does not support HTML5 video.
                             </video>
                         </div>
