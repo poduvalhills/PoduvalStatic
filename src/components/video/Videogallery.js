4ec947bc29067f75c99154d731e8317ed882cbf1
poduvalhills <56753005+poduvalhills@users.noreply.github.com>
2021-09-20 22:11:17 +0530

    Update Videogallery.js
    video  url update

--- src/components/video/Videogallery.js
+++ src/components/video/Videogallery.js
@@ -10,6 +10,7 @@ class Videogallery extends React.Component{
                         <div className="col-md-4 text-center">
                             <video width="400" controls style="display:none">                              
                               <source src={require('../media/checkdam.mp4')} type="video/mp4" />
+                              <source src="https://www.poduvalhills.com/static/media/checkdam.mp4" type="video/mp4" />
                               Your browser does not support HTML5 video.
                             </video>                            
                         </div>
@@ -18,6 +19,7 @@ class Videogallery extends React.Component{
                         <div className="col-md-4 text-center">
                             <video width="400" controls style="display:none">                              
                               <source src={require('../media/hills.mp4')} type="video/mp4" />
+                              <source src="https://www.poduvalhills.com/static/media/hills.mp4" type="video/mp4" />
                               Your browser does not support HTML5 video.
                             </video>                            
                         </div>
@@ -26,6 +28,7 @@ class Videogallery extends React.Component{
                         <div className="col-md-4 text-center">
                             <video width="400" controls style="display:none">                              
                               <source src={require('../media/views.mp4')} type="video/mp4" />
+                              <source src="https://www.poduvalhills.com/static/media/views.mp4" type="video/mp4" />
                               Your browser does not support HTML5 video.
                             </video>
                         </div>
