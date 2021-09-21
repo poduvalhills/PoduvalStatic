0439339b84f6f8d87a41951d927266c04f6be1c3
poduvalhills <56753005+poduvalhills@users.noreply.github.com>
2021-09-21 16:57:00 +0530

    Update Videogallery.js
    live url video tag added for testing purpose

--- src/components/video/Videogallery.js
+++ src/components/video/Videogallery.js
@@ -7,19 +7,28 @@ class Videogallery extends React.Component{
             <div>
                 <div className="row" id="videos">
                     <div className="col-md-4 text-center">
-                            <video width="400" controls>                              
+                            <video width="400" controls style="display:none">                              
                               <source src={require('../media/checkdam.mp4')} type="video/mp4" />
-                            </video>                            
+                            </video>
+                            <video width="400" controls>                              
+                              <source src="https://www.maxsee.in/poduvalhills/checkdam.mp4" type="video/mp4" />
+                            </video>                                                     
                     </div>
                     <div className="col-md-4 text-center">
-                        <video width="400" controls>                              
+                        <video width="400" controls style="display:none">                              
                           <source src={require('../media/hills.mp4')} type="video/mp4" />
-                        </video>                            
+                        </video>  
+                        <video width="400" controls>                              
+                           <source src="https://www.maxsee.in/poduvalhills/hills.mp4" type="video/mp4" />
+                        </video> 
                     </div>
                     <div className="col-md-4 text-center">
-                        <video width="400" controls>                              
+                        <video width="400" controls style="display:none">                              
                           <source src={require('../media/views.mp4')} type="video/mp4" />
                         </video>
+                        <video width="400" controls>                              
+                           <source src="https://www.maxsee.in/poduvalhills/views.mp4" type="video/mp4" />
+                        </video> 
                     </div>
                 </div>
                 <div className="row">
