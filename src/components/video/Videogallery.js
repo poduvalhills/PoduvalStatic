208ee1772671669bdbc49fb56ab0e11e808133d1
poduvalhills <56753005+poduvalhills@users.noreply.github.com>
2021-09-21 12:07:30 +0530

    Update Videogallery.js
    sulekha video source update

--- src/components/video/Videogallery.js
+++ src/components/video/Videogallery.js
@@ -9,18 +9,21 @@ class Videogallery extends React.Component{
                     <div className="col-md-4 text-center">
                             <video width="400" controls style="display:none">                              
                               <source src={require('../media/checkdam.mp4')} type="video/mp4" />
+                              <source src="https://www.poduvalhills.com/static/media/checkdam.mp4" type="video/mp4" />
                               Your browser does not support HTML5 video.
                             </video>                            
                     </div>
                     <div className="col-md-4 text-center">
                         <video width="400" controls style="display:none">                              
                           <source src={require('../media/hills.mp4')} type="video/mp4" />
+                          <source src="https://www.poduvalhills.com/static/media/hills.mp4" type="video/mp4" />
                           Your browser does not support HTML5 video.
                         </video>                            
                     </div>
                     <div className="col-md-4 text-center">
                         <video width="400" controls style="display:none">                              
                           <source src={require('../media/views.mp4')} type="video/mp4" />
+                          <source src="https://www.poduvalhills.com/static/media/views.mp4" type="video/mp4" />
                           Your browser does not support HTML5 video.
                         </video>
                     </div>
